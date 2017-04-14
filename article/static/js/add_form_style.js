bc339bbabd93c2b8c20c37e60e503c1b37fba4a7
Yuliya <skripka696@ukr.net>
2017-04-14 11:34:19 +0300

    add remove

--- article/static/js/add_form_style.js
+++ article/static/js/add_form_style.js
@@ -11,10 +11,29 @@ $(function(){
             $('input[name="form-MAX_NUM_FORMS"]', content).remove();
             var selects = $('select', content);
             console.log(selects);
+            $('.add_more').val('Remove').addClass('remove').removeClass('add_more');
+            $('.remove', content).val('Add More').addClass('add_more').removeClass('remove');
             for (var i=0; i< selects.length; i++){
+
                 $(selects[i]).attr('name', $(selects[i]).attr('name').replace(/\d+/g, forms_id-1));
                 $(selects[i]).attr('id', $(selects[i]).attr('id').replace(/\d+/g, forms_id-1));
             }
             $('form').append(content);
         });
+        $('form').on('click', "input.remove", function (e) {
+            var forms_id = parseInt($('#id_form-TOTAL_FORMS').val());
+            forms_id -= 1;
+            $('#id_form-TOTAL_FORMS').val(forms_id);
+            $(this).parent().remove();
+            var divs = $('div', 'form');
+            for (var i=0; i< divs.length; i++){
+                var selects = $('select', divs[i]);
+                for (var j=0; j< selects.length; j++){
+                    $(selects[j]).attr('name', $(selects[j]).attr('name').replace(/\d+/g, i));
+                    $(selects[j]).attr('id', $(selects[j]).attr('id').replace(/\d+/g, i));
+                }
+
+            }
+
+        })
     });
