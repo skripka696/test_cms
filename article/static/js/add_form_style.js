21d559ef6283a490cecdaef4e34224494af05cdd
Yuliya <skripka696@ukr.net>
2017-04-14 10:38:42 +0300

    fix add form

--- article/static/js/add_form_style.js
+++ article/static/js/add_form_style.js
@@ -5,10 +5,13 @@ $(function(){
             $('#id_form-TOTAL_FORMS').val(forms_id);
             var content = $('.form_content').clone();
             content.removeClass("form_content");
+            $('input[name="form-INITIAL_FORMS"]', content).remove();
+            $('input[name="form-TOTAL_FORMS"]', content).remove();
+            $('input[name="form-MIN_NUM_FORMS"]', content).remove();
+            $('input[name="form-MAX_NUM_FORMS"]', content).remove();
             var selects = $('select', content);
             console.log(selects);
             for (var i=0; i< selects.length; i++){
-                // var current_select = $(selects[i]).clone();
                 $(selects[i]).attr('name', $(selects[i]).attr('name').replace(/\d+/g, forms_id-1));
                 $(selects[i]).attr('id', $(selects[i]).attr('id').replace(/\d+/g, forms_id-1));
             }
