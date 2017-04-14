e781ed3734426ff0eeda326b45b177641399dcd8
TurboBelka <turbo.belka@bk.ru>
2017-04-14 15:30:21 +0300

    changed style options to name

--- article/static/js/get_style_options.js
+++ article/static/js/get_style_options.js
@@ -15,7 +15,7 @@ $(function(){
                 var select_for_option = $(current_select).parent().find('.style_option');
                 $(select_for_option).empty();
                 for (var i = 0; i < data['data'].length; i++) {
-                    var $new_option = $('<option value="' + data['data'][i].id + '">' + data['data'][i].value + '</option>');
+                    var $new_option = $('<option value="' + data['data'][i].id + '">' + data['data'][i].name + '</option>');
                     $(select_for_option).append($new_option);
                 }
                 $(select_for_option).removeAttr('disabled');
