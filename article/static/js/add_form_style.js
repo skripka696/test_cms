0d8db8f256883a3351b628674e9e9ad76c12e85d
Yuliya <skripka696@ukr.net>
2017-04-14 15:03:23 +0300

    fix add/remove form

--- article/static/js/add_form_style.js
+++ article/static/js/add_form_style.js
@@ -1,30 +1,28 @@
 $(function(){
+        var content = $('.form_content').clone();
+        content.removeClass("form_content");
         $('form').on('click', "input.add_more", function (e) {
             var forms_id = parseInt($('#id_form-TOTAL_FORMS').val());
             forms_id += 1;
             $('#id_form-TOTAL_FORMS').val(forms_id);
-            var content = $('.form_content').clone();
-            content.removeClass("form_content");
-            $('input[name="form-INITIAL_FORMS"]', content).remove();
-            $('input[name="form-TOTAL_FORMS"]', content).remove();
-            $('input[name="form-MIN_NUM_FORMS"]', content).remove();
-            $('input[name="form-MAX_NUM_FORMS"]', content).remove();
-            var selects = $('select', content);
-            console.log(selects);
+            var content1 = $(content).clone();
+            console.log(content1);
+            var selects = $('select', content1);
             $('.add_more').val('Remove').addClass('remove').removeClass('add_more');
-            $('.remove', content).val('Add More').addClass('add_more').removeClass('remove');
+            $('.remove', content1).val('Add More').addClass('add_more').removeClass('remove');
             for (var i=0; i< selects.length; i++){
 
                 $(selects[i]).attr('name', $(selects[i]).attr('name').replace(/\d+/g, forms_id-1));
                 $(selects[i]).attr('id', $(selects[i]).attr('id').replace(/\d+/g, forms_id-1));
             }
-            $('form').append(content);
+            $('.add_content').append(content1);
         });
         $('form').on('click', "input.remove", function (e) {
+
             var forms_id = parseInt($('#id_form-TOTAL_FORMS').val());
             forms_id -= 1;
             $('#id_form-TOTAL_FORMS').val(forms_id);
-            $(this).parent().remove();
+            $(this).parent().parent().remove();
             var divs = $('div', 'form');
             for (var i=0; i< divs.length; i++){
                 var selects = $('select', divs[i]);
