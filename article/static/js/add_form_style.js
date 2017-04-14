c41cc30ed4304748d2e8ca9faaeaca13743b6363
Yuliya <skripka696@ukr.net>
2017-04-14 10:29:28 +0300

    add form style js

--- article/static/js/add_form_style.js
+++ article/static/js/add_form_style.js
@@ -1,16 +1,16 @@
 $(function(){
-        $('#add_more').on('click', function (e) {
+        $('form').on('click', "input.add_more", function (e) {
             var forms_id = parseInt($('#id_form-TOTAL_FORMS').val());
             forms_id += 1;
-            total_form.val(forms_id);
+            $('#id_form-TOTAL_FORMS').val(forms_id);
             var content = $('.form_content').clone();
+            content.removeClass("form_content");
             var selects = $('select', content);
             console.log(selects);
             for (var i=0; i< selects.length; i++){
-                var current_select = $(selects[i]).clone();
-                $(current_select).attr('name', $(current_select).attr('name').replace(/\d+/g, forms_id-1));
-                $(current_select).attr('id', $(current_select).attr('id').replace(/\d+/g, forms_id-1));
-                // $('form').append(current_select);
+                // var current_select = $(selects[i]).clone();
+                $(selects[i]).attr('name', $(selects[i]).attr('name').replace(/\d+/g, forms_id-1));
+                $(selects[i]).attr('id', $(selects[i]).attr('id').replace(/\d+/g, forms_id-1));
             }
             $('form').append(content);
         });
