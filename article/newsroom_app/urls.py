0f6e66b3a0242558a9e0f63ebc5511734d202399
Yuliya <skripka696@ukr.net>
2017-04-19 10:43:41 +0300

    add url

--- article/newsroom_app/urls.py
+++ article/newsroom_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^create_style/$', views.CreateStyle.as_view(), name='add_style'),
     url(r'^get_style_options/(?P<id>\d+)/$', views.get_style_options, name='get_style_options'),
     url(r'^create_article/$', views.CreateArticle.as_view(), name='add_style'),
+    url(r'^get_field_style/$', views.get_field_style, name='get_field_style'),
 ]
