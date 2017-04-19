28e3874e039e7e1a38ed58373c9d7f10cca70f94
Yuliya <skripka696@ukr.net>
2017-04-19 15:48:17 +0300

    generate css file

--- article/newsroom_app/apps.py
+++ article/newsroom_app/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class NewsroomAppConfig(AppConfig):
     name = 'newsroom_app'
+
+    def ready(self):
+        import newsroom_app.signals
--- article/newsroom_app/signals.py
+++ article/newsroom_app/signals.py
@@ -1,9 +1,26 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, m2m_changed
 from django.dispatch import receiver
-from newsroom_app.models import Field
+from newsroom_app.models import Field, StyleValue
 from django.template import Template, Context
+from django.template.loader import get_template
+from article.settings import BASE_DIR
+import os
 
 
-@receiver(post_save, sender=Field)
+@receiver(m2m_changed, sender=Field.style.through)
+@receiver(m2m_changed, sender=StyleValue)
 def generate_css(instance, **kwargs):
-    pass
+    file_style_name = 'article_style.css'
+    style_template_dir = os.path.join(BASE_DIR, 'templates', 'style_template.html')
+    file_style_dir = os.path.join(BASE_DIR, 'static', file_style_name)
+    fields_style = Field.objects.all()
+
+    temp = get_template(style_template_dir)
+
+    my_template = temp.render({'selectors': fields_style})
+
+    print(kwargs['action'])
+    print(my_template)
+    filename = open(file_style_dir, 'w')
+    filename.write(my_template)
+    filename.close()
